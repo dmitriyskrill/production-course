65c2f29e039cbb9018540af562402199535a8ea6
Asus Vivobook <dmitriyskrill@gmail.com>
2023-01-30 22:11:14 +0300

    4 Подключаем React и настраиваем css в webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,18 @@ import { RuleSetRule } from 'webpack'
 
 export function buildLoaders (): RuleSetRule[] {
 
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader'
+    ]
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -9,6 +21,7 @@ export function buildLoaders (): RuleSetRule[] {
   }
 
   return [
-    typescriptLoader
+    typescriptLoader,
+    cssLoader
   ]
 }
