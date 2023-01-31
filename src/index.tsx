4d1c674854cf627d8ef031d1ee0c553393f05ef1
Asus Vivobook <dmitriyskrill@gmail.com>
2023-01-31 13:42:05 +0300

    7 Организация стилей. Добавляем темы. Переход на 17 реакт. За автором

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import {render} from "react-dom";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
-createRoot(
-  document.getElementById('root')
-).render(
+render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 )
