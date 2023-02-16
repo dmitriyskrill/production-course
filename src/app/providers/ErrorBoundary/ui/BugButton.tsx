a82df76b46f2acb31e0457aecf3fc5e3a5f80079
Asus Vivobook <dmitriyskrill@gmail.com>
2023-02-16 11:45:49 +0300

    22 ErrorBoundary. Обработка React ошибок

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import { Button } from 'shared/ui/Button/Button'
+import { useTranslation } from 'react-i18next'
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+  const { t } = useTranslation()
+  const [error, setError] = useState(false)
+  const onThrow = () => setError(true)
+
+  useEffect(() => {
+    if (error === true) throw new Error()
+  }, [error])
+  return (
+    <Button
+      onClick={onThrow}
+    >
+      {t('Вызвать ошибку для ErrorBoundary')}
+    </Button>
+  )
+}
---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,43 @@
+import React, { ErrorInfo, ReactNode, Suspense } from 'react'
+import { PageError } from 'widgets/PageError'
+// Если нужна обертка перевода для слассового компонента
+// import { withTranslation } from 'react-i18next'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: Boolean
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+    console.log(error, errorInfo)
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+    if (hasError) {
+      // Можно отрендерить запасной UI произвольного вида
+      return <Suspense fallback=""><PageError /></Suspense>
+    }
+
+    return children
+  }
+}
+// Если нужна обертка перевода для слассового компонента
+// export default withTranslation()(ErrorBoundary)
+export default ErrorBoundary
