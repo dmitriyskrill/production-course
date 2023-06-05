27a085e75751d3a16b45146e15f7980f16f262ce
dmitriyskrill <dmitriyskrill@gmail.com>
2023-06-05 18:51:55 +0300

    32 + 33 Кастомный input. Окно авторизации. Lazy modal + Husky

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Modal } from 'shared/ui/Modal/Modal';
-
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
