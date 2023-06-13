ae4c2d27050a30c4fdce02a3038d8410e59e8419
dmitriyskrill <dmitriyskrill@gmail.com>
2023-06-14 02:02:26 +0300

    36 Тестирование фичи authByUsername. TestAsyncThunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+  test('test set password', () => {
+    const state: DeepPartial<LoginSchema> = {
+      username: 'admin',
+    };
+    expect(loginReducer(state as LoginSchema, loginActions.setUsername('username'))).toEqual({ username: 'username' });
+  });
+  test('test set username', () => {
+    const state: DeepPartial<LoginSchema> = {
+      password: 'pass',
+    };
+    expect(loginReducer(state as LoginSchema, loginActions.setPassword('password'))).toEqual({ password: 'password' });
+  });
+});
